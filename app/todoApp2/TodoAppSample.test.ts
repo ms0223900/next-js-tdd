4a178b95e3f4b735b3df4682115cc3677a67d691
penguin-cho <penguin-cho@cloud-latitude.com>
2024-06-12 21:12:42 +0800

    feat: add todo

---
+++ app/todoApp2/TodoAppSample.test.ts
@@ -0,0 +1,53 @@
+class TodoList {
+    constructor(todos: Todo[]) {
+        this.todos = todos;
+    }
+
+    private todos: Todo[];
+
+    get() {
+        return this.todos;
+    }
+
+    addTodo(todo: Todo) {
+        this.todos.push(todo)
+    }
+}
+
+class TodoAppSampleTest {
+    constructor() {
+        this.todoList = new TodoList([]);
+    }
+
+    private todoList: TodoList;
+
+    addTodo(content: string) {
+        this.todoList.addTodo(new Todo(content))
+
+    }
+
+    getTodoList() {
+        return this.todoList.get();
+    }
+}
+
+class Todo {
+    content: string
+    checked: boolean;
+
+    constructor(content: string) {
+        this.checked = false
+        this.content = content
+    }
+
+}
+
+describe('Todo App TDD Sample', function () {
+    it('Should add todo', () => {
+        let todoAppSample = new TodoAppSampleTest();
+        todoAppSample.addTodo('hi')
+        expect(todoAppSample.getTodoList()).toEqual([
+            new Todo('hi')
+        ])
+    });
+});
