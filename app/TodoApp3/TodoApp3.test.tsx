b30836aede6fcfe93edb10ade5cc79dc363994e6
penguin-cho <penguin-cho@cloud-latitude.com>
2024-06-18 20:18:22 +0800

    feat: remove one todo

--- app/TodoApp3/TodoApp3.test.tsx
+++ app/TodoApp3/TodoApp3.test.tsx
@@ -1,4 +1,5 @@
 interface TodoItem {
+    id: number;
     checked: boolean
     content: string
 }
@@ -7,11 +8,13 @@ class TodoApp3 {
     constructor() {
         this.todoList = [];
     }
+
     private todoList: TodoItem[];
 
     addTodo(content: string) {
         this.todoList.push({
-            checked:false,
+            id:0,
+            checked: false,
             content,
         })
     }
@@ -19,15 +22,38 @@ class TodoApp3 {
     getTodoList() {
         return this.todoList;
     }
+
+    removeTodo(todoId: number) {
+        const newTodoList = this.todoList.filter(todo => todo.id!==todoId);
+        this.todoList=newTodoList
+    }
 }
 
 describe('Todo App Sample 3', function () {
+    let todoApp3 = new TodoApp3();
+    beforeEach(() => {
+         todoApp3 = new TodoApp3();
+    });
+
     it('should add todo', () => {
-        let todoApp3 = new TodoApp3();
         todoApp3.addTodo('hi')
         expect(todoApp3.getTodoList()).toEqual([{
-            checked:false,
+            id: 0,
+            checked: false,
             content: 'hi'
         }])
     });
+
+    it('should remove todo', () => {
+        todoApp3.addTodo('hi')
+        expect(todoApp3.getTodoList()).toEqual([{
+            id: 0,
+            checked: false,
+            content: 'hi'
+        }])
+        todoApp3.removeTodo(0)
+        expect(todoApp3.getTodoList()).toHaveLength(0)
+    });
+
+
 });
