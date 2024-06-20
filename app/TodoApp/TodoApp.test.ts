b92ce61873608d69e128f80b009e2cb3261e97b5
penguin-cho <penguin-cho@cloud-latitude.com>
2024-06-20 20:52:30 +0800

    feat: add two todos

--- app/TodoApp/TodoApp.test.ts
+++ app/TodoApp/TodoApp.test.ts
@@ -1,22 +1,37 @@
+interface TodoItem {
+    checked: boolean
+    content: string
+}
+
 class TodoApp {
+    private todos: TodoItem[];
+
+
+    constructor() {
+        this.todos = [];
+    }
+
     addTodo(content: string) {
+        this.todos.push({
+            checked: false,
+            content,
+        })
 
     }
 
     getTodos() {
-        return [
-            {
-                checked: false,
-                content: '- [ ] Add todo, and get todo'
-            }
-        ];
+        return this.todos;
 
     }
 }
 
 describe('Todo App', function () {
+    let todoApp = new TodoApp();
+    beforeEach(() => {
+        todoApp = new TodoApp();
+    });
+
     it('Should add todo, and get one todo.', () => {
-        let todoApp = new TodoApp();
         todoApp.addTodo('- [ ] Add todo, and get todo')
         expect(todoApp.getTodos()).toEqual([
             {
@@ -25,4 +40,21 @@ describe('Todo App', function () {
             }
         ])
     });
+
+    it('Should add two todos, and get two todos.', () => {
+        todoApp.addTodo('- [ ] Add todo, and get todo')
+        todoApp.addTodo('- [ ] Add two todos')
+        expect(todoApp.getTodos()).toEqual([
+            {
+                checked: false,
+                content: '- [ ] Add todo, and get todo'
+            },
+            {
+                checked: false,
+                content: '- [ ] Add two todos'
+            }
+        ])
+    });
+
+
 });
